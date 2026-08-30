11e8a16e0ce46d3531fd5e08417b3841be6c59aa
mazzdhoo94-creator <mazzdhoo94@gmail.com>
2026-08-30 17:06:39 +0700

    🔄 Bot: Added new bot token to object database

--- Token.js
+++ Token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8609664888:AAG4AAbgPb5iEhTsq0Mqp5v2AyA3YvTv1wU"
+    "8609664888:AAG4AAbgPb5iEhTsq0Mqp5v2AyA3YvTv1wU",
+    "8471922173:AAH_q8rbXI1kSpsL93eERpsZnYtPXOI47lk"
   ]
 }
