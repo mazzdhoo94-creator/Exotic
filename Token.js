f0b20a78e1d9a623579907dc09e83f3149c73f77
mazzdhoo94-creator <mazzdhoo94@gmail.com>
2026-08-30 17:08:02 +0700

    🔄 Bot: Removed a bot token from object database

--- Token.js
+++ Token.js
@@ -1,6 +1,5 @@
 {
   "tokens": [
-    "8609664888:AAG4AAbgPb5iEhTsq0Mqp5v2AyA3YvTv1wU",
-    "8471922173:AAH_q8rbXI1kSpsL93eERpsZnYtPXOI47lk"
+    "8609664888:AAG4AAbgPb5iEhTsq0Mqp5v2AyA3YvTv1wU"
   ]
 }
